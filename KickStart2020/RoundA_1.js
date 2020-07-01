beb0b44472702e7ab3bda171a4de38454e94032a
Bankdevelop <thitiwat.ku@ku.th>
2020-07-01 20:15:24 +0700

    Update kickStart_roundA_1

--- KickStart2020/RoundA_1.js
+++ KickStart2020/RoundA_1.js
@@ -1,5 +1,4 @@
 const readline = require('readline');
-const { WSAEHOSTDOWN } = require('constants');
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -11,63 +10,79 @@ var input = [], output = [], canBuy = 0;
 var nHouse = 0, budget = 0, testRound = 1;
 
 //input
-rl.on("line", function(line) {
+(new Promise((resolve, reject) => {
+		rl.on("line", function(line) {
 
-    if(n==0) n=parseInt(line)*2;
-   
-    if(i%2==0) {
-        arr = line.split(" ");
-        nHouse = parseInt(arr[0]);
-        budget = parseInt(arr[1]);
-    }else{
-        houses = line.split(" ");
-        e = 0;
-        while( e++ < nHouse ) {
-          num = parseInt(houses[e]);
-          if(input.length == 0) input.push(num);
-          else {
-            if( num >= input[e-1] ){
-              input.push(num);
-            }else{
-              j = e;
-              while(j>0){
-                if(num > input[j-1]){
-                  g = input.length;
-                  while(g>j-1){
-                    input[g] = input[g-1];
-                    g--;
-                  }
-                  input[j-1] = num;
-                  break;
-                }
-                j--;
-              }
-              if(j==0) input = [num, ...input];
-            }
-          }
-        }
-        //output
-        let p = 0;
-        while( p < nHouse ){
-          if( (result=budget-input[p++])>=0 ) {
-            budget=result;
-            canBuy++;
-          }else{
-            break;
-          }
-        }
+			if(n===0) n=parseInt(line)*2;
+			else{
+				if(i%2===0) {
+					arr = line.split(" ");
+					nHouse = parseInt(arr[0]);
+					budget = parseInt(arr[1]);
+				}else{
+					houses = line.split(" ");
+					nInput = 0;
+					//insertion sort
+					while( nInput < nHouse ) {
+						num = parseInt(houses[nInput]);
+						if(input.length === 0) {
+							input.push(num);
+						} else {
+							if( num >= input[nInput-1] ){
+								input.push(num);
+							}else{
+								j = nInput-1;
+								while(j>0){
+									if(num > input[j-1]){
+										g = nInput;
+										while(g>j){
+											input[g] = input[g-1];
+											g--;
+										}
+										input[j] = num;
+										break;
+									}
+									j--;
+								}
+								if(j===0) input = [num, ...input];
+							}
+							console.log(input);
+						}
+						nInput++;
+					}
 
-        console.log(`Case #${testRound}: ${canBuy}`);
-        output.push(`Case #${testRound++}: ${canBuy}`);
-    }
-    if(i++>=n) rl.close();
+					//output
+					let p = 0;
+					while( p < nHouse ){
+						if( (result=budget-input[p++])>=0 ) {
+							budget=result;
+							canBuy++;
+						}else{
+							break;
+						}
+					}
+					
+					output.push(`Case #${testRound++}: ${canBuy}`);
+				}
+
+				if(++i>=n) {
+					resolve();
+					rl.close();
+				}
+
+				//reset value
+				input = [], canBuy = 0;
+			}
+			
+		});
+	})
+).then(()=>{
+	var nOutput = output.length;
+	var k = 0;
+	while( k < nOutput ){
+	  console.log(output[k]);
+	  k++;
+	}
 });
 
-var nOutput = output.length;
-var k = 0;
-while( k < nOutput ){
-  console.log(output[k]);
-  k++;
-}
-
-//asynchronous problem all while loop run at the same time
+//result : Sample Failed: WA
